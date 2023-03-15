55926aacee394d16d4962bf10c9196c1cc429f5e
Naveenguptha <naveenguptha347@gmail.com>
2023-03-15 22:22:57 +0530

    Service Example...

---
+++ core/src/main/java/com/adobe/aem/guides/bright/core/models/SeabrightServiceDemo.java
@@ -0,0 +1,10 @@
+package com.adobe.aem.guides.bright.core.models;
+
+import com.day.cq.wcm.api.Page;
+
+import java.util.Iterator;
+
+public interface SeabrightServiceDemo {
+    public Iterator<Page> getPagesList();
+}
+ 	
